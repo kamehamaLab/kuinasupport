a5acedae443fe6d4f9956f7e399e392d51bcbad1
maitake4211 <dmwajtgp4211@gmail.com>
2022-04-25 22:42:21 +0900

    add:とりあえずドライブにpushはできた

---
+++ python/drivepush.py
@@ -0,0 +1,15 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
+
+JSON_FILE = "secret/service_account.json"
+ID = "1ohXp1eD2onXEhNzhk19WefYaaQenYQJF"
+
+gauth = GoogleAuth()
+scope = ["https://www.googleapis.com/auth/drive"]
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(JSON_FILE, scope)
+drive = GoogleDrive(gauth)
+
+file = drive.CreateFile({"title": "test.txt", "parents": [{"id": ID}]})
+file.SetContentString("テスト")
+file.Upload()
