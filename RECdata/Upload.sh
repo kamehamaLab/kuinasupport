702a67ce1a8997ca9a818c5d7d86bf8efe95a9e9
hkamehama <kamehamalab@gmail.com>
2022-04-27 17:46:43 +0900

    giginoreを反映

--- RECdata/Upload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-if [ -e *.wav  ]; then
-    OLD_FILE="`pwd`/`ls -ltr *.wav | head -n 1 | awk '{print $9}'`"
-    rclone copy $OLD_FILE GoogleDrive:
-    echo "wavファイルのアップロード完了"
-    rm $OLD_FILE
-else
-    echo  "wavファイルが存在しません。"
-fi
