f6aaaf82e56a9649249674bf4d492c96b32134c2
maitake4211 <dmwajtgp4211@gmail.com>
2022-05-02 00:57:20 +0900

    fix:broken pipeの解決

--- UploadtoGoogleDrive.py
+++ UploadtoGoogleDrive.py
@@ -6,32 +6,35 @@ import csv
 import time
 from GoogleDrivefunc import getGoogleService, uploadFileToGoogleDrive
 
+def main():
+    keyFile = "client_secret.json" # ドライブに接続するためのjson設定ファイル
+    dirname = "RECdatas/"
 
-keyFile = "client_secret.json" # ドライブに接続するためのjson設定ファイル
-dirname = "RECdatas/"
+    #あとから値を代入する変数郡
+    fileID = ""
+    fileName = ""
 
-#あとから値を代入する変数郡
-fileID = ""
-fileName = ""
+    try:
+        while True:
+            files = os.listdir(dirname)
+            if len(files) > 0:
+                files.sort()
+                filepath = dirname + files[0]
 
-try:
-    while True:
-        files = os.listdir(dirname)
-        if len(files) > 0:
-            files.sort()
-            filepath = dirname + files[0]
-
-            getGoogleService(keyFile)
-            fileID = uploadFileToGoogleDrive(files[0], filepath, keyFile)
-            with open('Logs/UploadLog.csv', 'a') as f:
-                writer = csv.writer(f)
-                writer.writerow([files[0], fileID])
-            os.remove(filepath)
-        else :
-            time.sleep(100)
-            print("wait")
+                getGoogleService(keyFile)
+                fileID = uploadFileToGoogleDrive(files[0], filepath, keyFile)
+                with open('Logs/UploadLog.csv', 'a') as f:
+                    writer = csv.writer(f)
+                    writer.writerow([files[0], fileID])
+                os.remove(filepath)
+            else :
+                time.sleep(100)
+                print("wait")
 
 
 
-except KeyboardInterrupt:
-    print("Ctrl+C finished")
+    except KeyboardInterrupt:
+        print("Ctrl+C finished")
+
+if __name__ == "__main__":
+    main()
