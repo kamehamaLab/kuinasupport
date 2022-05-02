b190a69ff30533abe9cdeda05267be2e6c55cd7b
maitake4211 <dmwajtgp4211@gmail.com>
2022-05-02 16:02:05 +0900

    fix:broken pipe error対策

--- CutOnGoogleDrive.py
+++ CutOnGoogleDrive.py
@@ -10,25 +10,36 @@ from GoogleDrivefunc import getGoogleService, deletefileinGoogleDrive, getlistGo
 keyFile = "client_secret.json"
 savedir = "RECdatas/"
 
-try:
+def main():
+    getGoogleService(keyFile)
+    list = getlistGoogleDrive(keyFile)
+    if len(list["files"]) > 0:
+        fileID = list["files"][0]["id"]
+        fileName = list["files"][0]["name"]
+        downloadtoGoogleDrive(fileID, fileName, savedir, keyFile)
+
+        deletefileinGoogleDrive(fileID, keyFile)
+        with open('Logs/deleteLog.csv', 'a') as f:
+            dt_now = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
+            writer = csv.writer(f)
+            writer.writerow([fileName, dt_now])
+
+    else:
+        print("wait")
+        time.sleep(3000)
+
+
+
+
+if __name__ == "__main__":
     while True:
-        getGoogleService(keyFile)
-        list = getlistGoogleDrive(keyFile)
-        if len(list["files"]) > 0:
-            fileID = list["files"][0]["id"]
-            fileName = list["files"][0]["name"]
-            downloadtoGoogleDrive(fileID, fileName, savedir, keyFile)
+        try:
+            main()
 
+        except KeyboardInterrupt:
+            print("Ctrl+C finished")
+            break
 
-            deletefileinGoogleDrive(fileID, keyFile)
-            with open('Logs/deleteLog.csv', 'a') as f:
-                dt_now = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
-                writer = csv.writer(f)
-                writer.writerow([fileName, dt_now])
-
-        else:
-            print("wait")
-            time.sleep(3000)
-
-except KeyboardInterrupt:
-    print("Ctrl+C finished")
+        except BrokenPipeError:
+            print("BrokenPipeError")
+            print("reconnect")
