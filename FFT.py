e1f119e886a7382433cb6dcafb1bcac483293831
maitake4211 <dmwajtgp4211@gmail.com>
2022-05-24 20:23:16 +0900

    add:FFT.py

---
+++ FFT.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pydub import AudioSegment
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 音楽データの読み込み
+sound = AudioSegment.from_file("RECdata/GallirallusOkinawae.mp3", "mp3")
+
+# NumPy配列に返還
+data = np.array(sound.get_array_of_samples())
+
+# ステレオ音声から片方を抽出
+x = data[::sound.channels]
+
+# グラフ化
+plt.plot(x[::10])
+plt.grid()
+plt.show()
--- SoundClassification.py
+++
@@ -1,9 +0,0 @@
-import librosa
-import matplotlib.pyplot as plt
-if __name__ == "__main__":
-    wave, fs = wav_read(path_to_wavefile)
-    rms = librosa.feature.rms(y=wave) #音量の計算
-    times = librosa.times_like(rms, sr=fs) #時間軸の生成
-    plt.plot(times, rms[0]*2**(1/2)) #rms➡振幅に変換
-    plt.show()
-    plt.close()
