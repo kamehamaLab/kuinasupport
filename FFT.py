fc04f28e9d2a9de2b337ca07e37290869df547aa
maitake4211 <dmwajtgp4211@gmail.com>
2022-05-25 12:58:26 +0900

    fix:とりあえずのFFTをした

--- FFT.py
+++ FFT.py
@@ -1,18 +1,24 @@
-# -*- coding: utf-8 -*-
-from pydub import AudioSegment
+import sys
+import scipy.io.wavfile
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 音楽データの読み込み
-sound = AudioSegment.from_file("RECdata/GallirallusOkinawae.mp3", "mp3")
 
-# NumPy配列に返還
-data = np.array(sound.get_array_of_samples())
+#音声ファイル読み込み
+args = sys.argv
+wav_filename = args[1]
+rate, data = scipy.io.wavfile.read(wav_filename)
 
-# ステレオ音声から片方を抽出
-x = data[::sound.channels]
 
-# グラフ化
-plt.plot(x[::10])
-plt.grid()
+#（振幅）の配列を作成
+data = data / 32768
+
+##### 周波数成分を表示する #####
+#縦軸：dataを高速フーリエ変換する（時間領域から周波数領域に変換する）
+fft_data = np.abs(np.fft.fft(data))
+#横軸：周波数の取得　　#np.fft.fftfreq(データ点数, サンプリング周期)
+freqList = np.fft.fftfreq(data.shape[0], d=1.0/rate)
+#データプロット
+plt.plot(freqList, fft_data)
+plt.xlim(0, 25000) #0～8000Hzまで表示
 plt.show()
