0cafbaf171418a4b0eae8c54cd38636a7e7a7363
maitake4211 <dmwajtgp4211@gmail.com>
2022-06-08 17:37:26 +0900

    fix:音声データの情報を色々表示できるように変更

--- SoundWaveView.py
+++ SoundWaveView.py
@@ -8,4 +8,7 @@ a, sr = librosa.load("AudioSamples/"+filename+".mp3")
 librosa.display.waveshow(a, sr=sr)
 print(type(a)) #type:numpy.ndarray
 print(type(sr)) #type:int
+print( 'a length :%d' % len(a))
+print('Sampling rate (Hz): %d' % sr)
+print('Audio length (seconds): %.2f' % (len(a) / sr))
 plt.show()
