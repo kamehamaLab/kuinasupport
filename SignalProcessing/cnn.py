ec211ba99defaf15889114eb0a7e6199b10712fc
maitake4211 <dmwajtgp4211@gmail.com>
2022-07-05 19:27:01 +0900

    update:グラフの結果更新＆CNNのコード改良

--- SignalProcessing/cnn.py
+++ SignalProcessing/cnn.py
@@ -27,20 +27,20 @@ def plot_history(history, save_graph_img_path, fig_size_width, fig_size_height, 
     plt.figure(figsize=(fig_size_width, fig_size_height))
     plt.rcParams['font.family'] = 'Times New Roman'
     plt.rcParams['font.size'] = lim_font_size  # 全体のフォント
-    #plt.subplot(121)
+    plt.subplot(121)
 
     # plot accuracy values
     plt.plot(epochs, acc, color = "blue", linestyle = "solid", label = 'train acc')
     plt.plot(epochs, val_acc, color = "green", linestyle = "solid", label= 'valid acc')
-    #plt.title('Training and Validation acc')
-    #plt.grid()
-    #plt.legend()
+    plt.title('Training and Validation acc')
+    plt.grid()
+    plt.legend()
 
     # plot loss values
-    #plt.subplot(122)
+    plt.subplot(122)
     plt.plot(epochs, loss, color = "red", linestyle = "solid" ,label = 'train loss')
-    #plt.plot(epochs, val_loss, color = "orange", linestyle = "solid" , label= 'valid loss')
-    #plt.title('Training and Validation loss')
+    plt.plot(epochs, val_loss, color = "orange", linestyle = "solid" , label= 'valid loss')
+    plt.title('Training and Validation loss')
     plt.legend()
     plt.grid()
 
