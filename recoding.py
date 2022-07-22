2386511ba7b06b86d283e6ce03b36576ca855b08
maitake4211 <dmwajtgp4211@gmail.com>
2022-07-22 10:58:46 +0900

    fix:メイン文でループが起こるように変更

--- recoding.py
+++ recoding.py
@@ -10,46 +10,45 @@ chunk = 4096 # 2^12 一度に取得するデータ数
 record_secs = 180 # 録音する秒数
 dev_index = 2 # デバイス番号
 
-def REC(stream):
-    dt_now = datetime.datetime.now()
-    wav_output_filename = ("RECdatas/" + dt_now.strftime('%Y_%m_%d-%H_%M_%S') + ".wav")
+def main(stream):
+    audio = pyaudio.PyAudio() # create pyaudio instantiation
     # create pyaudio stream
-    print("recording")
-    frames = []
+    stream = audio.open(format = form_1, rate = samp_rate, channels = chans, input_device_index = dev_index,input = True, frames_per_buffer=chunk)
+    print("connected")
+    while True:
+        dt_now = datetime.datetime.now()
+        wav_output_filename = ("RECdatas/" + dt_now.strftime('%Y_%m_%d-%H_%M_%S') + ".wav")
+        print("recording")
+        frames = []
+        # loop through stream and append audio chunks to frame array
+        for i in range(0,int((samp_rate/chunk)*record_secs)):
+        	data = stream.read(chunk)
+        	frames.append(data)
+        stream.stop_stream()
+        audio.terminate()
+        print("finished recording")
 
-    # loop through stream and append audio chunks to frame array
-    for i in range(0,int((samp_rate/chunk)*record_secs)):
-    	data = stream.read(chunk)
-    	frames.append(data)
+        wavefile = wave.open(wav_output_filename,'wb')
+        wavefile.setnchannels(chans)
+        wavefile.setsampwidth(audio.get_sample_size(form_1))
+        wavefile.setframerate(samp_rate)
+        wavefile.writeframes(b''.join(frames))
+        wavefile.close()
 
-    print("finished recording")
+        with open('Logs/RecodingLog.csv', 'a') as f:
+            writer = csv.writer(f)
+            writer.writerow([wav_output_filename])
 
     # stop the stream, close it, and terminate the pyaudio instantiation
     stream.stop_stream()
     stream.close()
     audio.terminate()
 
-    # save the audio frames as .wav file
-    wavefile = wave.open(wav_output_filename,'wb')
-    wavefile.setnchannels(chans)
-    wavefile.setsampwidth(audio.get_sample_size(form_1))
-    wavefile.setframerate(samp_rate)
-    wavefile.writeframes(b''.join(frames))
-    wavefile.close()
-
-    with open('Logs/RecodingLog.csv', 'a') as f:
-        writer = csv.writer(f)
-        writer.writerow([wav_output_filename])
-
-
 
 if __name__ == "__main__":
-    audio = pyaudio.PyAudio() # create pyaudio instantiation
-    # create pyaudio stream
-    stream = audio.open(format = form_1, rate = samp_rate, channels = chans, input_device_index = dev_index,input = True, frames_per_buffer=chunk)
     while True:
         try:
-            REC(stream)
+            main()
 
         except KeyboardInterrupt:
             print("Ctrl+C finished")
