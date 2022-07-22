137a9ae11e8a53715523dd1022139a63d4a5285a
maitake4211 <dmwajtgp4211@gmail.com>
2022-07-22 13:42:26 +0900

    fix

--- tempRecive.py
+++ tempRecive.py
@@ -3,8 +3,8 @@ import csv
 
 ser = serial.Serial('/dev/ttyACM0', 115200)
 while True:
-    String_data = ser.readline()
-    print(str(String_data))
+    String_data = str(ser.readline())
+    print(String_data)
     with open('Logs/temp.csv', 'a') as f:
         writer = csv.writer(f)
         writer.writerow(String_data)
