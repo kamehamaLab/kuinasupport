d4734fd78e4f9d3d9098aefd2713b6486cef47f9
maitake4211 <dmwajtgp4211@gmail.com>
2022-07-22 13:39:18 +0900

    fix

--- tempRecive.py
+++ tempRecive.py
@@ -3,4 +3,7 @@ ser = serial.Serial('/dev/ttyACM0', 115200)
 while True:
     String_data = ser.readline()
     print(String_data)
+    with open('Logs/temp.csv', 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(String_data)
 ser.close()
