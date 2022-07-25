e53cb2f5d5adb89a906b6cb511ad32b715ff4901
maitake4211 <dmwajtgp4211@gmail.com>
2022-07-25 21:38:29 +0900

    fix:この出力からindex番号引っ張ってくるように仕様変更しよう

--- deviceshow.py
+++ deviceshow.py
@@ -2,3 +2,5 @@ import pyaudio
 p = pyaudio.PyAudio()
 for i in range(p.get_device_count()):
     print(p.get_device_info_by_index(i))
+
+#{'index': 0, 'structVersion': 2, 'name': 'Microsoft サウンド マッパー - Input', 'hostApi': 0, 'maxInputChannels': 2, 'maxOutputChannels': 0, 'defaultLowInputLatency': 0.09, 'defaultLowOutputLatency': 0.09, 'defaultHighInputLatency': 0.18, 'defaultHighOutputLatency': 0.18, 'defaultSampleRate': 44100.0}
