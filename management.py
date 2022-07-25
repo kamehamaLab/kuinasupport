16a39332c23b9347e4bc961aafa52866c03ece51
maitake4211 <dmwajtgp4211@gmail.com>
2022-07-25 18:34:20 +0900

    add写真を保存するフォルダを追加

--- management.py
+++ management.py
@@ -14,13 +14,15 @@ while True:
         dt_now_str = dt_now.strftime('%Y_%m_%d-%H_%M_%S')
         writer = csv.writer(f)
         writer.writerow([dt_now_str,str(float_data)])
+        print("save temp")
 
     camera.start_preview()
     sleep(5)#このスリープは少なくとも2秒必要。カメラの露光時間が必要なため
     dt_now_str = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
     camera.capture('images/image' + dt_now_str + '.jpg')
+    print("imageSaved")
     camera.stop_preview()
 
-    sleep(3600)
+    sleep(10)
 
 ser.close()
