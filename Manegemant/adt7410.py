01cb8515e0d936cbd3f4b28d6a24b1972e36bae0
maitake4211 <dmwajtgp4211@gmail.com>
2022-09-21 15:11:49 +0900

    feet:新しいセンサをマネジメントに実装

--- Manegemant/adt7410.py
+++ Manegemant/adt7410.py
@@ -5,15 +5,8 @@ address_adt7410 = 0x48
 register_adt7410 = 0x00
 configration_adt7410 = 0x03
 
-# 13bitに設定して読み出し
 bus.write_word_data(address_adt7410, configration_adt7410, 0x00)
 word_data = bus.read_word_data(address_adt7410, register_adt7410)
-
-# ２バイトの入れ替え
 data = (word_data & 0xff00) >> 8 | (word_data & 0xff) << 8
-
-# 3ビットの右シフト
 data = data >> 3
-
-# 16で割って温度に
 print(data/16.)
--- Manegemant/management.py
+++ Manegemant/management.py
@@ -1,26 +1,33 @@
-import serial
 import csv
 import datetime
 from picamera import PiCamera
 from time import sleep
+import smbus
 
 camera = PiCamera()
-ser = serial.Serial('/dev/ttyACM0', 115200)
+bus = smbus.SMBus(1)
+address_adt7410 = 0x48
+register_adt7410 = 0x00
+configration_adt7410 = 0x03
+
 while True:
-    String_data = ser.readline().strip()
-    float_data = float(String_data)
+    bus.write_word_data(address_adt7410, configration_adt7410, 0x00)
+    word_data = bus.read_word_data(address_adt7410, register_adt7410)
+    data = (word_data & 0xff00) >> 8 | (word_data & 0xff) << 8
+    data = data >> 3
+    data = data/16.
     with open('Logs/temp.csv', 'a') as f:
         dt_now = datetime.datetime.now()
         dt_now_str = dt_now.strftime('%Y_%m_%d-%H_%M_%S')
         writer = csv.writer(f)
-        writer.writerow([dt_now_str,str(float_data)])
-        print("save temp")
+        writer.writerow([dt_now_str,str(data)])
+        #print("save temp")
 
     camera.start_preview()
     sleep(5)#このスリープは少なくとも2秒必要。カメラの露光時間が必要なため
     dt_now_str = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
     camera.capture('images/image' + dt_now_str + '.jpg')
-    print("imageSaved")
+    #print("imageSaved")
     camera.stop_preview()
 
     sleep(10)
