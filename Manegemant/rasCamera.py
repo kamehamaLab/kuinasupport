bfcd42be063a9178d8e7b22d4620c5e4fccfe2b0
maitake4211 <dmwajtgp4211@gmail.com>
2022-09-28 13:44:31 +0900

    いらないコード削除

--- Manegemant/rasCamera.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-
-camera.start_preview()
-sleep(5)#このスリープは少なくとも2秒必要。カメラの露光時間が必要なため
-camera.capture('image.jpg')
-camera.stop_preview()
