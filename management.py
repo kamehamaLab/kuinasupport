f4c3af1f0ad1d783dab309acb7dd45bf8d1ccb23
maitake4211 <dmwajtgp4211@gmail.com>
2022-09-28 13:47:19 +0900

    fix:コメント編集

--- management.py
+++ management.py
@@ -28,7 +28,7 @@ while True:
         #print("save temp")
 
     camera.start_preview()
-    sleep(5)#このスリープは少なくとも2秒必要。カメラの露光時間が必要なため
+    sleep(5) # このスリープは少なくとも2秒必要。カメラの露光時間が必要なため
     dt_now_str = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
     camera.capture('images/image' + dt_now_str + '.jpg')
     #print("imageSaved")
