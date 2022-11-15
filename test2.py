b3e0fe64e119f69de256198a380f4620f191be69
maitake4211 <dmwajtgp4211@gmail.com>
2022-11-15 16:42:09 +0900

    testコードを編集

--- test2.py
+++ test2.py
@@ -1,19 +1,14 @@
-import pyocr as ocr
-import pyocr.builders
-from PIL import Image
+import os
+from PIL import Image, ImageFilter
+import pyocr
+import csv
+import datetime
+from picamera import PiCamera
+from time import sleep
 
-def main():
-	img_path = "test.png"
-
-	tools = pyocr.get_available_tools()
-	if len(tools) == 0:
-		print("No OCR tool found")
-		sys.exit(1)
-
-	tool = tools[0]
-	res = tool.image_to_string(Image.open(img_path),
-							builder=pyocr.builders.TextBuilder(tesseract_layout=6))
-	print(res)
-
-if __name__ == '__main__':
-	main()
+camera = PiCamera()
+camera.start_preview()
+sleep(5) # このスリープは少なくとも2秒必要。カメラの露光時間が必要なため
+dt_now_str = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
+img_name = 'images/image' + dt_now_str + '.jpg'
+camera.capture(img_name)
