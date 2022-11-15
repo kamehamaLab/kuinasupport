65e999abc53300cdcd612ed78911825dbbcd5373
maitake4211 <dmwajtgp4211@gmail.com>
2022-11-15 14:49:03 +0900

    8割かんせい

--- ocr.py
+++ ocr.py
@@ -18,7 +18,10 @@ def clip_img(img):
     im_crop4 = img.crop(a, b, c, d)
     im_crop5 = img.crop(a, b, c, d)
     im_crop6 = img.crop(a, b, c, d)
-    return (im_crop1, im_crop2, im_crop3, im_crop4, im_crop5, im_crop6)
+    im_crop7 = img.crop(a, b, c, d)
+    im_crop8 = img.crop(a, b, c, d)
+    im_crop9 = img.crop(a, b, c, d)
+    return (im_crop1, im_crop2, im_crop3, im_crop4, im_crop5, im_crop6, im_crop7, im_crop8, im_crop9)
 
 #環境変数「PATH」にTesseract-OCRのパスを設定。
 #Windowsの環境変数に設定している場合は不要。
@@ -57,17 +60,20 @@ print(text)
 def main():
     img = Image.open('images/dataset/0/0-2.jpg')
     img = img.filter(ImageFilter.GaussianBlur(radius=2))
-    clim1, clim2, clim3, clim4, clim5, clim6 = clip_img(img)
+    clim1, clim2, clim3, clim4, clim5, clim6, clim7, clim8, clim9 = clip_img(img)
     text1 = tool.image_to_string(clim1, lang="eng", builder=builder)
     text2 = tool.image_to_string(clim2, lang="eng", builder=builder)
     text3 = tool.image_to_string(clim3, lang="eng", builder=builder)
     text4 = tool.image_to_string(clim4, lang="eng", builder=builder)
     text5 = tool.image_to_string(clim5, lang="eng", builder=builder)
     text6 = tool.image_to_string(clim6, lang="eng", builder=builder)
-    with open('Logs/deleteLog.csv', 'a') as f:
+    text7 = tool.image_to_string(clim7, lang="eng", builder=builder)
+    text8 = tool.image_to_string(clim8, lang="eng", builder=builder)
+    text9 = tool.image_to_string(clim9, lang="eng", builder=builder)
+    with open('Logs/batteryLog.csv', 'a') as f:
             dt_now = datetime.datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
             writer = csv.writer(f)
-            writer.writerow([dt_now, ])
+            writer.writerow([dt_now, (text1+text2+text3), (text4+text5+text6), (text7+text8+text9)])
 
 
 
